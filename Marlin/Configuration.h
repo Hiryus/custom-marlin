b51a7a8392a10ff7292caceff91ded9c860f0347
Hiryus <hiryus.sha@gmail.com>
2020-01-31 12:59:29 +0100

    Enable BLTOUCH

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -77,7 +77,7 @@
 */
 //#define ABL_EZABL // TH3D EZABL or Any NO Sensor
 //#define ABL_NCSW //Creality ABL or Any NC Sensor
-//#define ABL_BLTOUCH
+#define ABL_BLTOUCH
 
 //#define CREALITY_ABL_MOUNT //Using creality ABL mount
 //#define E3D_DUALFAN_MOUNT // Using HD Modular mount as above with 2 5015 blowers and sensor on the right
